0d1b40504b871bb95bdc6f044cb86ef60c0a177a
VanyaMark <markova.vanya@gmail.com>
2024-05-16 19:01:16 +0200

    done

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // ¡Buena suerte!
+const adviceId = document.getElementById("advice-id");
+const cardQuote = document.querySelector(".card__quote");
+const dice = document.querySelector(".card__dice");
+
+dice.addEventListener("click", async ()=> {
+    const response = await fetch("https://api.adviceslip.com/advice");
+
+    const data = await response.json();
+    console.log(data);
+
+    adviceId.innerHTML = data.slip.id;
+    cardQuote.innerHTML = data.slip.advice;
+})
